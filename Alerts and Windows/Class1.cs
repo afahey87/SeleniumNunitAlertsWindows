c950792e260edf63a2822ece88411ae9ea436890
Adam Fahey <afahey87@gmail.com>
2017-10-07 15:13:07 -0400

    Setup

---
+++ Alerts and Windows/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alerts_and_Windows
+{
+    class Class1
+    {
+    }
+}
---
+++ Alerts and Windows/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Alerts_and_Windows
+{
+
+    [TestFixture]
+    class WebDriverTestAlertsandWindows
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://thetestroom.com/webapp");
+        }
+        [TearDown]
+        public void teardown()
+        {
+            driver.Close();
+        }
+        [Test]
+        public void shouldOpenTermsWindowAndCloseIt()
+        {
+            driver.FindElement(By.Id("footer_term")).Click();
+            
+        }
+
+        [Test]
+        public void shouldBeAbleToClosePopup()
+        {
+
+        }
+    }
+}
